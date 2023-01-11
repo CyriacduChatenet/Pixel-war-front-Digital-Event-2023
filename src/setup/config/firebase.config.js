3297e61b46f255b155b231b74efba437eb1f8e89
Théo Gillet <90395803+theo-code33@users.noreply.github.com>
2023-01-11 19:18:53 +0100

    [ADD] Add game service

--- src/setup/config/firebase.config.js
+++ src/setup/config/firebase.config.js
@@ -2,12 +2,12 @@ const { initializeApp } = require('firebase/app');
 const { getFirestore } = require('firebase/firestore');
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAET9nyNsU_kTbuG5jMFFJxAn3S2XuXU2o",
-    authDomain: "pixel-war-esd-thomas.firebaseapp.com",
-    projectId: "pixel-war-esd-thomas",
-    storageBucket: "pixel-war-esd-thomas.appspot.com",
-    messagingSenderId: "735644169756",
-    appId: "1:735644169756:web:7c80c4445272d1545a51d9"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
