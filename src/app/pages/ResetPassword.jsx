4fdca85fbc1a6a3ed0e13efad5b71776db0e17cb
NicoooM <nmarsan33000@gmail.com>
2023-01-12 18:14:30 +0100

    [UPDATE] errors reset password

--- src/app/pages/ResetPassword.jsx
+++ src/app/pages/ResetPassword.jsx
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import { resetPassword } from "../../setup/utils/useApi";
 
 const ResetPassword = () => {
+  const [result, setResult] = useState("");
   const [credentials, setCredentials] = useState("");
 
   const handleChange = (e) => {
@@ -11,11 +12,36 @@ const ResetPassword = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    resetPassword(credentials);
+    resetPassword(credentials)
+      .then(() => {
+        setResult(
+          "Un email de réinitialisation de mot de passe a été envoyé à l'adresse indiquée"
+        );
+      })
+      .catch((err) => {
+        setResult(
+          "Une erreur est survenue, veuillez vérifier votre adresse mail avant de renvoyer une demande"
+        );
+      });
   };
+
+  const renderResult = () => {
+    if (result !== "") {
+      if (
+        result ===
+        "Un email de réinitialisation de mot de passe a été envoyé à l'adresse indiquée"
+      ) {
+        return <p className="l-reset__success">{result}</p>;
+      } else {
+        return <p className="l-reset__error">{result}</p>;
+      }
+    }
+  };
+
   return (
     <div className="l-reset">
       <h1>Tu as oublié ton mot de passe ?</h1>
+      {renderResult()}
       <form onSubmit={handleSubmit}>
         <input
           type="email"
